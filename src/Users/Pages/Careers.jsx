37004919fe27d9d1877e30448a1860312f432dab
anagha2307 <anaghavarghese100@gmail.com>
2025-09-29 12:03:47 +0530

    Day6 Design: Careers Modal and Profile Page

--- src/Users/Pages/Careers.jsx
+++ src/Users/Pages/Careers.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Components/Header'
 import Footer from '../../Components/Footer'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLocationDot, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
+import { faLocationDot, faArrowUpRightFromSquare, faXmark } from '@fortawesome/free-solid-svg-icons'
 
 const Careers = () => {
+  const [modalStatus, setModalstatus] = useState(true)
   return (
     <>
       <Header />
@@ -22,9 +23,9 @@ const Careers = () => {
           <div className="flex justify-between items-center m-5">
             <h1 className='text-xl p-5 text-gray-700 font-bold'>Job Title</h1>
             <div className="flex">
-            <button className="bg-blue-800 text-white text-lg rounded m-5 md:px-4 px-2 py-2">Apply
-              <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='ms-2 text-2xl'  />
-            </button>
+              <button className="bg-blue-800 text-white text-lg rounded m-5 md:px-4 px-2 py-2">Apply
+                <FontAwesomeIcon icon={faArrowUpRightFromSquare} className='ms-2 text-2xl' />
+              </button>
             </div>
           </div>
           <hr className='text-gray-300 mx-3' />
@@ -32,22 +33,65 @@ const Careers = () => {
             <div className="flex gap-1">
               <FontAwesomeIcon icon={faLocationDot} className='text-blue-700' />
               <h1>Location</h1>
-            </div>      
+            </div>
             <h1>Job Type: <span>Senior Level</span></h1>
             <h1>Salary: <span>10 Lakhs</span></h1>
             <h1>Qualification: <span>M-Tech/B-Tech/MCA/BCA</span></h1>
             <h1>Experience: <span>5-7 years</span></h1>
             <h1>Description: <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque nesciunt porro illo reiciendis, repellat veritatis debitis voluptatibus ut, natus quia dolor nostrum est veniam alias explicabo repudiandae tempora neque quaerat!</span></h1>
           </div>
-          
-
-
-
         </div>
 
 
 
       </div>
+      {/* modal */}
+      {
+        <div className="relative z-10 overflow-y-auto">
+          <div className="bg-gray-500/75 fixed inset-0">
+            <div className="flex justify-center items-center min-h-screen scroll-auto">
+              <div className="bg-white rounded-2xl w-150">
+                {/* modal header*/}
+                <div className="bg-black text-white flex justify-between items-center p-3 text-xl">
+                  <h3>Application Form</h3>
+                  <FontAwesomeIcon onClick={() => setModalstatus(false)} icon={faXmark} />
+                </div>
+                {/* modal body*/}
+                <div className="relative p-5">
+                  <div className="md:grid grid-cols-2 gap-2">
+                    <div className="mb-3 col-span-1">
+                      <input type="text" placeholder='Name' className='border p-2 w-full rounded placeholder-gray-600 text-black' />
+                    </div>
+                    <div className="mb-3">
+                      <input type="text" placeholder='Email Id' className='border w-full p-2 rounded placeholder-gray-600 text-black' />
+                    </div>
+                    <div className="mb-3">
+                      <input type="text" placeholder='Qualification' className='border w-full p-2 rounded placeholder-gray-600 text-black' />
+                    </div>
+                    <div className="mb-3">
+                      <input type="text" placeholder='Phone' className='border w-full p-2 rounded placeholder-gray-600 text-black' />
+                    </div>
+                    <div className="mb-3 col-span-2">
+                      <textarea type="text" placeholder='Coverletter' className='border w-full p-2 rounded placeholder-gray-600 text-black' />
+                    </div>
+                  </div>
+                  <div className="mb-3 col-span-2 flex">
+                    <input type="text" placeholder='Resume' className='border border-gray-300 p-2 bg-gray-600 placeholder-white text-black w-24' />
+                    <input type="file" placeholder='Name' className='border border-gray-400 p-2 w-full placeholder-gray-600 text-black' />
+                  </div>
+                </div>
+                {/* modal footer*/}
+                <div className="bg-gray-400 text-white flex justify-end items-center p-3 text-xl">
+                  <h3>Application Form</h3>
+                </div>
+
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      }
 
       <Footer />
 
--- src/Users/Pages/Profile.jsx
+++ src/Users/Pages/Profile.jsx
@@ -1,10 +1,168 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Header from '../Components/Header'
+import Footer from '../../Components/Footer'
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Profile = () => {
+  const [sellBookStatus, setSellBookStatus] = useState(true)
+  const [bookStatus, setBookStatus] = useState(false)
+  const [purchaseStatus, setPurchaseStatus] = useState(true)
+
   return (
-    <div>
-      Profile
-    </div>
+    <>
+      <Header />
+      <div style={{ height: '200px' }} className="bg-black"></div>
+      <div className="bg-white p-3"></div>
+      <div className="md:flex justify-between px-20 mt-5">
+        <div className="flex items-center">
+          <h1 className="font-bold md:text-3xl text-2xl">Username</h1>
+          <FontAwesomeIcon className='text-blue-400 ms-3 text-2xl' icon={faCircleCheck} />
+        </div>
+        <div>Edit</div>
+      </div>
+      <p className="md:px-20 px-5 my-5 text-justify">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores vel reprehenderit voluptatem sequi possimus, corrupti tempora, nobis exercitationem dolorem illum aperiam architecto. Perferendis optio minus illo molestias iure, magni quibusdam?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores vel reprehenderit voluptatem sequi possimus, corrupti tempora, nobis exercitationem dolorem illum aperiam architecto. Perferendis optio minus illo molestias iure, magni quibusdam?
+      </p>
+      <div className="md:px-40">
+        {/* tabs */}
+        <div className="flex justify-center items-center my-8 font-medium text-lg">
+          <p onClick={() => { setSellBookStatus(true); setPurchaseStatus(false); setBookStatus(false) }}
+            className={sellBookStatus ? 'text-blue-500 p-4 border-gray-200 border-t border-l border-r rounded cursor-pointer' : 'p-4 border border-b border-gray-200 cursor-pointer'}>Sell Books</p>
+          <p onClick={() => { setBookStatus(true); setPurchaseStatus(false); setSellBookStatus(false) }}
+            className={bookStatus ? 'text-blue-500 p-4 border-gray-200 border-t border-l border-r rounded cursor-pointer' : 'p-4 border border-b border-gray-200 cursor-pointer'}>Book Status</p>
+          <p onClick={() => { setPurchaseStatus(true); setSellBookStatus(false); setBookStatus(false) }}
+            className={purchaseStatus ? 'text-blue-500 p-4 border-gray-200 border-t border-l border-r rounded cursor-pointer' : 'p-4 border border-b border-gray-200 cursor-pointer'}>Purchase History</p>
+        </div>
+      </div>
+      {/* contents */}
+      {/* Sell Books */}
+      {
+        sellBookStatus &&
+        <div>
+          <div className="p-10 my-20 mx-30 bg-gray-200">
+            <div className="text-center text-3xl font-medium">Book Details</div>
+            <div className="md:grid grid-cols-2 mt-10 w-full">
+              <div className="px-3">
+                <div className="mb-3 px-3">
+                  <input type="text" placeholder='Title' className='w-full p-2 rounded placeholder-gray-600 text-black bg-white' />
+                </div>
+                <div className="mb-3 px-3">
+                  <input type="text" placeholder='Author' className='w-full p-2 rounded placeholder-gray-600 text-black bg-white' />
+                </div>
+                <div className="mb-3 px-3">
+                  <input type="text" placeholder='' className='w-full p-2 rounded placeholder-gray-600 text-black bg-white' />
+                </div>
+                <div className="mb-3 px-3">
+                  <input type="text" placeholder='' className='w-full p-2 rounded placeholder-gray-600 text-black bg-white' />
+                </div>
+                <div className="mb-3 px-3">
+                  <input type="text" placeholder='No.of Pages' className='w-full p-2 rounded placeholder-gray-600 bg-white text-black' />
+                </div>
+                <div className="mb-3 px-3">
+                  <input type="text" placeholder='ISBN' className='w-full p-2 rounded placeholder-gray-600 text-black bg-white' />
+                </div>
+                <div className="mb-3 px-3">
+                  <input type="text" placeholder='Image URL' className='w-full p-2 rounded placeholder-gray-600 bg-white text-black' />
+                </div>
+                <div className="mb-3 px-3">
+                  <textarea type="text" rows={'5'} placeholder='Abstract' className='w-full p-2 rounded placeholder-gray-600 bg-white text-black' />
+                </div>
+              </div>
+              <div className="px-3">
+                <div className="mb-3 px-3">
+                <input type="text" placeholder='Publisher' className='w-full p-2 rounded bg-white placeholder-gray-600 text-black' />
+              </div>
+              <div className="mb-3 px-3">
+                <input type="text" placeholder='Language' className='w-full p-2 rounded bg-white placeholder-gray-600 text-black' />
+              </div>
+              <div className="mb-3 px-3">
+                <input type="text" placeholder='Category' className='w-full p-2 rounded bg-white placeholder-gray-600 text-black' />
+              </div>
+              <div className="mb-3 px-3">
+                <input type="text" placeholder='Category' className='w-full p-2 rounded bg-white placeholder-gray-600 text-black' />
+              </div>
+              <div className="mb-3 flex justify-center items-center mt-10">
+                <label htmlFor="bookImage"></label>
+                <img src="https://cdn.pixabay.com/photo/2016/01/03/00/43/upload-1118929_640.png" alt="book image" height={'200px'} width={'200px'} />
+              </div>
+
+
+              </div>
+            </div>
+            <div className="p-3 w-full flex md:justify-end justify-center gap-5">
+                <button className="px-4 py-2 bg-gray-600 text-white ">Reset</button>
+                <button className="p-3 bg-gray-600 text-white">Submit</button>
+              </div>
+          </div>
+        </div>
+      }
+      {/* Book Status*/}
+      {
+        bookStatus &&
+        <div className="p-10 m-20 shadow rounded">
+          {/* duplicate div according to book */}
+          <div className="p-5 rounded mt-4 bg-gray-100">
+            <div className="md:grid grid-cols-[3fr_1fr]">
+              <div className="px-4">
+                <h1 className="text-2xl">Book Title</h1>
+                <h1 className="text-xl">Author</h1>
+                <h1 className="text-lg">$ 300</h1>
+                <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quibusdam consequuntur modi reprehenderit tempore, recusandae voluptatibus neque, quod temporibus commodi minus voluptas, laudantium veniam cupiditate iure cum molestias? Ipsam, nemo!</p>
+                <div className="flex">
+                  <img height={'150px'} width={'150px'} src="https://psdstamps.com/wp-content/uploads/2022/04/round-pending-stamp-png.png" alt="pending img" />
+                  <img height={'150px'} width={'100px'}  src="https://pngimg.com/uploads/approved/approved_PNG13.png" alt="" />
+                </div>
+              </div>
+              <div className="px-4 mt-4 md:mt-0">
+                <img width={'100%'} src="/View.jpg" alt="book" />
+                <div className="mt-4 flex justify-end">
+                  <button className="p-3 bg-red-600 text-white ">Delete</button>
+                </div>
+                 
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      }
+
+      {
+        purchaseStatus &&
+        <div className="p-10 m-20 shadow rounded">
+          {/* duplicate div according to book */}
+          <div className="p-5 rounded mt-4 bg-gray-100">
+            <div className="md:grid grid-cols-[3fr_1fr]">
+              <div className="px-4">
+                <h1 className="text-2xl">Book Title</h1>
+                <h1 className="text-xl">Author</h1>
+                <h1 className="text-lg">$ 300</h1>
+                <p className="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil quibusdam consequuntur modi reprehenderit tempore, recusandae voluptatibus neque, quod temporibus commodi minus voluptas, laudantium veniam cupiditate iure cum molestias? Ipsam, nemo!</p>
+                <div className="flex">
+                  <img height={'150px'} width={'150px'} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUc-s1TQp1Z9hUJ2QbX5LXsUYpvNgI4i9Fzw&s" alt="pending img" />
+              </div>    
+              </div>
+              <div className="px-4 mt-4 md:mt-0">
+                <img width={'100%'} src="/View.jpg" alt="book" />
+                <div className="mt-4 flex justify-end">
+                  <button className="p-3 bg-red-600 text-white ">Delete</button>
+              </div>
+                 
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      }
+
+
+
+
+      <Footer />
+    </>
   )
 }
 
